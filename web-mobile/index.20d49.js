c33c10a957983fdcfc736f2312a389c95de771f4
Tu Luong <tu.luong@museajourney.com>
2025-12-15 12:52:22 +0700

    -deploy build

--- web-mobile/index.20d49.js
+++ web-mobile/index.20d49.js
@@ -1,4 +1,4 @@
-System.register(["./application.dac99.js"], function (_export, _context) {
+System.register(["./application.cc965.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
