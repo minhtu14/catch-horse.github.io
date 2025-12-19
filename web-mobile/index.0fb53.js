7c68894f09ec199d0385a9932f5a5b70f6d706af
Tu Luong <tu.luong@amanotes.com>
2025-12-19 20:46:40 +0700

    deploy new build

--- web-mobile/index.0fb53.js
+++ web-mobile/index.0fb53.js
@@ -1,4 +1,4 @@
-System.register(["./application.67fbc.js"], function (_export, _context) {
+System.register(["./application.5614f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
